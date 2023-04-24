c08ed04fdc2aa81ee74e5721a19ebab59c56fee0
Audrius Pasvenskas <apasvenskas@outlook.com>
2023-04-24 09:56:55 -0700

    work in progress

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './less/index.less'
 
 // Your code goes here!
+// First rvent load!!!
+window.onload = function (evt){
+    console.log(`${evt.type} started!`); //load to make sure nothing startes before DOM is loaded. 
+    const heading = document.querySelector('h1');
+    heading.textContent = 'Ready To Go';
+    // additional funtionality added when somthing is copied it will apear next tp h1.
+    window.addEventListener('copy', () => {
+        navigator.clipboard.readText()
+            .then(text => {
+                heading.textContent += text;
+            })
+    })
+// adding a click event!! 
+document.body.addEventListener('click', evt => {
+    evt.target.classList.toggle('mirror')
+}) 
+}
+
+
