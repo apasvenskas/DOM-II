88670dae8cbdf47c66166b13e1e09508a48ea471
Audrius Pasvenskas <apasvenskas@outlook.com>
2023-04-25 00:26:28 -0700

    project complete

--- src/index.js
+++ src/index.js
@@ -17,6 +17,31 @@ window.onload = function (evt){
 document.body.addEventListener('click', evt => {
     evt.target.classList.toggle('mirror')
 }) 
+
+// dblclick
+document.body.addEventListener('dblclick', evt => {
+    evt.target.outerHTML = ''
+}) // item disapears upon duoble clicking. 
+// keydown
+window.addEventListener('keydown', evt => {
+    if (evt.key == 6) {
+        document.body.innerHTML = '<h1>You been Hacked</h1>'
+    }
+})
+//records mose movment
+document.body.addEventListener('mousemove', evt => {
+    const { clientX, clientY} = evt
+    console.log(`mouse is at ${clientX}, ${clientY}`)
+})
+const destinations = document.querySelectorAll('.destination')//sleceting the class to be worked on.
+for (let destination of destinations) {
+    destination.addEventListener('mouseenter', evt => {
+        destination.style.fontWeight = 'bold'
+    })
+    destination.addEventListener('mouseleave', () => {
+        destination.style.fontWeight = 'initial'
+    })
+}
 }
 
 
